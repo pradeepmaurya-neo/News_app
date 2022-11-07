2a92673ebb21dcfa2603f4064124032ffb2c395c
Pradeep Maurya <pm9231@gmail.com>
2022-11-07 16:02:37 +0530

    project setup

---
+++ db.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import declarative_base, create_session, sessionmaker, scoped_session
+from sqlalchemy import create_engine
+from app import app
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+Base = declarative_base()
+
+engine = create_engine(os.environ.get('URL'))
+
+Session = sessionmaker(bind=engine)
+session = scoped_session(Session)
